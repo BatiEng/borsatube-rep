7c167e42415d2e744e1614d70fa21dd561d5da5f
Ali Batikan Ocal <alibatikan@gmail.com>
2025-01-07 16:00:56 +0300

    Added .env

--- vite-project/src/News.jsx
+++ vite-project/src/News.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 function News() {
   const [articles, setArticles] = useState([]);
-
+  const apiUrl = import.meta.env.VITE_API_URL;
   const formatDate = (date) => {
     const day = String(date.getDate()).padStart(2, "0"); // Adds leading zero if necessary
     const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based, so add 1
@@ -16,7 +16,7 @@ function News() {
 
   const getArticles = async () => {
     try {
-      const res = await axios.get("https://borsatube-rep-server.vercel.app/");
+      const res = await axios.get(apiUrl);
       setArticles(res.data);
     } catch (err) {
       console.log(err);
