d155061957ff29af3e707b5cb01e489c3ef32b41
Ali Batikan Ocal <alibatikan@gmail.com>
2025-01-09 20:15:48 +0300

    updated

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -4,96 +4,87 @@ import { AiOutlineStock } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
-import { useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function Header() {
-  document.addEventListener("click", (e) => {
-    const w = window.innerWidth;
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+  const navRef = useRef(null);
 
-    if (w < 880) {
-      if (
-        e.target.className === "hero-container" ||
-        e.target.className === "hero-title" ||
-        e.target.className === "tutorial-container" ||
-        e.target.className === "tutorial-container-sub" ||
-        e.target.className === "section-title"
-      ) {
-        const navEl = document.querySelector(".nav-links");
-        navEl.style.display = "none";
+  const toggleSidebar = () => {
+    setIsSidebarVisible((prev) => !prev);
+  };
+
+  const hideSidebar = () => {
+    setIsSidebarVisible(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setIsSidebarVisible(false);
       }
-      console.log(e.target.className);
-    }
-  });
-  const hideSideBar = () => {
-    const w = window.innerWidth;
+    };
 
-    if (w < 880) {
-      const navEl = document.querySelector(".nav-links");
-      navEl.style.display = "none";
-    }
-  };
-  const showSideBar = () => {
-    const navEl = document.querySelector(".nav-links");
-    navEl.style.display = "flex";
-  };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       <div className="header-container">
         <div className="header-logo">
           <Link to="/">
-            <img
-              className="header-logo"
-              src="https://github.com/BatiEng/borsatube-rep/blob/main/vite-project/public/borsatube-logo.png?raw=true"
-            />
+            <img className="header-logo" src="./../public/borsatube-logo.png" />
           </Link>
           <Link className="header-title" to="/">
             Borsatube
           </Link>
         </div>
-        <ul className="nav-links">
-          <li onClick={hideSideBar}>
+        <ul
+          ref={navRef}
+          className={`nav-links ${isSidebarVisible ? "visible" : "hidden"}`}
+        >
+          <li onClick={hideSidebar}>
             <p className="hide-icon">
               <IoClose />
             </p>
           </li>
-          <li onClick={hideSideBar}>
+          <li onClick={hideSidebar}>
             <Link to="/hakkimizda">
               <p>Hakkımızda</p>
             </Link>
           </li>
-          <li onClick={hideSideBar}>
+          <li onClick={hideSidebar}>
             <Link to="/analizler">
               <p>Analizler</p>
             </Link>
           </li>
-          <li onClick={hideSideBar}>
+          <li onClick={hideSidebar}>
             <Link to="/egitimler">
-              <p className="abc">Eğitimler</p>
+              <p>Eğitimler</p>
             </Link>
           </li>
-          {/* <li onClick={hideSideBar}>
+          <li onClick={hideSidebar}>
             <Link to="/etkinlikler">
               <p>Seminerler</p>
             </Link>
-          </li> */}
-          {/* <li onClick={hideSideBar}>
-            <Link to="/kutuphane">
-              <p>Kütüphane</p>
-            </Link>
-          </li> */}
-          <li onClick={hideSideBar}>
+          </li>
+          <li onClick={hideSidebar}>
             <Link to="/haberler">
               <p>Haberler</p>
             </Link>
           </li>
-          <li className="login-sidebar" onClick={hideSideBar}>
-            <Link to="/giris-yap">
+          <li className="login-sidebar" onClick={hideSidebar}>
+            <Link to="/login">
               <p>Giriş Yap</p>
             </Link>
           </li>
         </ul>
         <div className="header-bar-login">
-          <div onClick={showSideBar} className="bar-icon">
+          <div onClick={toggleSidebar} className="bar-icon">
             <FaBars />
           </div>
           <Link className="login" to="/giris-yap">
